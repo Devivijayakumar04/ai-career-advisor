d95726165182ac716121b5be15a6e09d075b0dd7
Devivijayakumar04 <devivijayakumar404@gmail.com>
2026-07-06 23:23:32 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -1,19 +1,10 @@
 from crewai import Agent
-from crewai.llms import LiteLLM
 
 def create_career_agent():
-    
-    llm = LiteLLM(
-        model="openrouter/openrouter/free",
-        api_key=os.getenv("OPENROUTER_API_KEY"),
-        api_base="https://openrouter.ai/api/v1"
-    )
-
     return Agent(
         role="Senior Career Counselor",
         goal="Provide 3 clear, personalized career recommendations",
-        backstory="You are an expert career advisor.",
-        llm=llm,   # ✅ IMPORTANT
-        verbose=False,
+        backstory="You are an expert career advisor helping students.",
+        verbose=True,
         allow_delegation=False
     )
